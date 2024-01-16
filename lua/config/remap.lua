bc981b84c5710f7eadfe32dcdaacfdc3d40369ca
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2024-01-16 23:51:32 +0100

    Added 2 custom keymaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -10,3 +10,6 @@ vim.keymap.set("n", "<C-Right>", ":bnext")
 vim.keymap.set("n", "<C-Left>", ":bprev")
 
 vim.keymap.set("i", "<C-c>", "<ESC>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
