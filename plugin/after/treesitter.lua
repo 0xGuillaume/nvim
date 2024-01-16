3b9dc0a0e45c97de46168baf65eab4e544aab634
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2024-01-16 23:51:11 +0100

    Removed help parser

--- plugin/after/treesitter.lua
+++ plugin/after/treesitter.lua
@@ -1,7 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or all (the five listed parsers should always be installed)
   ensure_installed = {
-	  "help",
 	  "dockerfile",
 	  "go",
 	  "hcl",
