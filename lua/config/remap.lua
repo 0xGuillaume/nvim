cebd740cb9fafa3e037b54f6e278e90ef4ba46ab
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2024-01-16 22:20:04 +0100

    Added custom remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,1 +1,12 @@
-print("hello from remap")
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("n", "oo", "o<ESC>")
+vim.keymap.set("n", "OO", "O<ESC>")
+vim.keymap.set("n", "Y", "y$")
+
+vim.keymap.set("n", "<C-Right>", ":bnext")
+vim.keymap.set("n", "<C-Left>", ":bprev")
+
+vim.keymap.set("i", "<C-c>", "<ESC>")
