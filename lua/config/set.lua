560afe7d267c0b7a758de29131d126d9e7c3da09
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2024-01-16 21:40:55 +0100

    Added vim set options

---
+++ lua/config/set.lua
@@ -0,0 +1,16 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+sewapfile = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
