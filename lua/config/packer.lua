ffa8d065391ba167159f19433b23e2ac2136f781
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2024-01-17 19:13:36 +0100

    Reformated lua & added plugins

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -1,37 +1,38 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file can be loaded by calling `lua require("plugins")` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd.packadd('packer.nvim')
+vim.cmd.packadd("packer.nvim")
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
 	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+	use "wbthomason/packer.nvim"
+	use "tpope/vim-dadbod"
 
 	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.3',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
+		"nvim-telescope/telescope.nvim", tag = "0.1.3",
+		-- or                            , branch = "0.1.x",
+		requires = { {"nvim-lua/plenary.nvim"} }
 	}
 
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"})
 
 	use { "catppuccin/nvim", as = "catppuccin" }
 
 	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
 		requires = {
 			--- Uncomment the two plugins below if you want to manage the language servers from neovim
 			---     --- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+			{"williamboman/mason.nvim"},
+			{"williamboman/mason-lspconfig.nvim"},
 
 			-- LSP Support
-			{'neovim/nvim-lspconfig'},
+			{"neovim/nvim-lspconfig"},
 			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
+			{"hrsh7th/nvim-cmp"},
+			{"hrsh7th/cmp-nvim-lsp"},
+			{"L3MON4D3/LuaSnip"},
 		}
 	}
 
