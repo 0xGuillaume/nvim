604be9b3f10aa49a7c726d0456ade0c1648869bd
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2024-01-17 11:52:51 +0100

    Configured catppuccin

---
+++ plugin/after/colors.lua
@@ -0,0 +1,11 @@
+require("catppuccin").setup {
+	color_overrides = {
+		all = {
+			base = "#000000",
+			mantl = "#000000",
+			crust = "#000000",
+		}
+	}
+}
+
+vim.cmd.colorscheme "catppuccin-mocha"
