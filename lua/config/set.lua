b32a36b473cff2f630c70ae8472d88ca464f58f7
0xGuillaume <salvor-tech@pm.me>
2024-09-14 21:36:21 +0200

    Added vim shortcuts

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,11 +1,22 @@
+-- Setting the default shell.
+vim.opt.shell = "/bin/fish"
+
+-- Configuring cursor shape.
 vim.opt.guicursor = ""
 
+-- Add numbers to the file.
 vim.opt.nu = true
+
+-- Add relative numbers to the file.
 vim.opt.relativenumber = true
 
+-- Set tabstop to 4 spaces.
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+
+-- Set shiftwidth to 4 columns.
 vim.opt.shiftwidth = 4
+
+-- Convert tabs to spaces.
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -15,4 +26,13 @@ vim.opt.swapfile = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Set an additionnal space on the number's left side.
+vim.opt.signcolumn = true
+
 vim.opt.termguicolors = true
+
+-- File browsing settings (:Explore)
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_showhide = 1
+vim.g.netrw_windowsize = 19
