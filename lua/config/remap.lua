f925c4403eccfc2c150eedd64edefda61b450854
0xGuillaume <salvor-tech@pm.me>
2024-09-14 21:36:08 +0200

    Added vim shortcuts

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,14 +2,11 @@ vim.g.mapleader = " "
 
 -- Normal mode
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "J", "mzJ`z")
 
 vim.keymap.set("n", "oo", "o<ESC>")
 vim.keymap.set("n", "OO", "O<ESC>")
 vim.keymap.set("n", "Y", "y$")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -18,13 +15,19 @@ vim.keymap.set("n", "<C-Left>", ":bprev")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- Navigate the split view easier by pressing:
+-- CTRL+j, CTRL+k, CTRL+h, or CTRL+l.
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
+-- Resize split windows using arrow keys by pressing:
+-- CTRL+UP, CTRL+DOWN, CTRL+LEFT, or CTRL+RIGHT.
+vim.api.nvim_set_keymap('n', '<C-Up>', '<C-w>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Down>', '<C-w>-', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Left>', '<C-w><', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Right>', '<C-w>>', { noremap = true, silent = true })
 
 -- Insert mode
 vim.keymap.set("i", "<C-c>", "<ESC>")
-
--- Visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
